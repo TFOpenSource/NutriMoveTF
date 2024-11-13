8f35292680a015d4c279f3cda76139953648738f
Jesus <jesus.cont5648@gmail.com>
2024-11-13 02:50:13 -0500

    feature dashboard has been fixed

--- src/app/Nutrimove/Features/pages/home-content/home-content.component.ts
+++ src/app/Nutrimove/Features/pages/home-content/home-content.component.ts
@@ -1,18 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from '../../../../shared/model/User/user.entity';
 import {NgIf} from '@angular/common';
 import {AuthenApiService} from '../../../Access/services/authen-api.service';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {DashboardService} from '../../services/dashboard.service';
 import {TranslateModule} from '@ngx-translate/core';
+import {MatButton} from '@angular/material/button';
+import {MatInput} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatProgressBar} from '@angular/material/progress-bar';
+import {EditGoalDialogComponent} from '../../components/edit-goal-dialog/edit-goal-dialog.component';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-home-content',
   standalone: true,
   imports: [
+    MatNativeDateModule,
+    EditGoalDialogComponent,
+    MatDialogModule,
     NgIf,
     MatProgressSpinner,
-    TranslateModule
+    TranslateModule,
+    MatButton,
+    MatInput,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatProgressBar,
   ],
   templateUrl: './home-content.component.html',
   styleUrl: './home-content.component.css'
@@ -23,6 +44,9 @@ export class HomeContentComponent implements OnInit {
   progress_cal = 10;
   progress_sleep = 0;
 
+  updatingHours = 0;
+  updatingHdr = 0;
+  qualitySleep: string = "";
 
   GOAL_HYDRATION: number = 2500;
   GOAL_SLEEP: number = 8;
@@ -32,7 +56,10 @@ export class HomeContentComponent implements OnInit {
   latestHydrationData: any | null = null;
   latestSleepData: any | null = null;
 
-  constructor(private authenService: AuthenApiService, private dashboardService: DashboardService) {
+  goal: any = null;
+  selectedGoal: any = null;
+
+  constructor(private authenService: AuthenApiService, private dashboardService: DashboardService, private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -40,56 +67,151 @@ export class HomeContentComponent implements OnInit {
       (user) => {
         this.currentUser = user;
         this.user_id = user?.id;
-        console.log(this.currentUser?.id);
-        console.log('Usuario autenticado en Home:', this.currentUser);
-
+        console.log('Usuario autenticado en Home:', this.currentUser?.name);
         this.getLatestHydrationForLast24Hours();
         this.getLatestSleepForLast24Hours()
       },
       error => {
         console.error('Error fetching current user:', error);
       }
-
     );
 
-
+    this.dashboardService.getGoal(this.user_id).subscribe(goalData => {
+      this.goal = goalData;
+    }, error => {
+      console.error('error loading goal:', error);
+      this.goal = null;
+      }
+    );
 
   }
 
-  private getLatestHydrationForLast24Hours(): void {
-    if (this.user_id) {
-      this.dashboardService.getLatestHydrationLast24Hours(parseInt(String(this.user_id))).subscribe(
-        data => {
-          this.latestHydrationData = data;
-          this.progress_hyd = this.latestHydrationData.quantity_ml * 100 / this.GOAL_HYDRATION;
-          console.log(this.progress_hyd);
-        },
-        error => {
-          console.error('Error fetching hydration data:', error);
+  openEditGoalModal(): void {
+    if (this.goal) {
+
+      this.selectedGoal = { ...this.goal };
+
+
+      const dialogRef = this.dialog.open(EditGoalDialogComponent, {
+        width: '400px',
+        data: { ...this.selectedGoal }
+      });
+
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+
+          result.start_date = this.formatDate(result.start_date);
+          result.end_date = this.formatDate(result.end_date);
+
+          this.goal = result;
+          const data = {
+            id:0,
+            user_id: this.user_id,
+            goal_type: this.goal.goal_type,
+            start_date: this.goal.start_date,
+            end_date: this.goal.end_date
+          };
+
+          this.dashboardService.updateGoal(this.goal.id, data).subscribe(
+            updatedGoal => {
+              console.log('Meta actualizada:', updatedGoal);
+              this.goal = updatedGoal;
+            },
+            error => {
+              console.error('Error al actualizar la meta:', error);
+            }
+          );
         }
-      );
-    } else {
-      console.warn('user_id is null or undefined');
+      });
     }
-
   }
+
+
+  private formatDate(date: Date): string {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    const day = d.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
 
   private getLatestSleepForLast24Hours(): void {
     if (this.user_id) {
-      this.dashboardService.getLatestSleepLast24Hours(parseInt(String(this.user_id))).subscribe(
-        data => {
-          this.latestSleepData = data;
-          this.progress_sleep = this.latestSleepData.hours_slept * 100 / this.GOAL_SLEEP;
-          console.log(this.progress_sleep);
+      this.dashboardService.getLatestSleepLast24Hours(this.user_id).subscribe(
+        totalHoursSlept => {
+          if (totalHoursSlept !== null && totalHoursSlept !== undefined) {
+            this.latestSleepData = totalHoursSlept;
+            this.progress_sleep = (this.latestSleepData * 100) / this.GOAL_SLEEP;
+          } else {
+            console.warn("No se obtuvieron datos recientes de sueño en las últimas 24 horas.");
+            this.progress_sleep = 0;
+          }
         },
         error => {
-          console.error('Error fetching hydration data:', error);
+          console.error('Error al obtener datos de sueño:', error);
+          this.progress_sleep = 0;
         }
       );
     } else {
+      console.warn('user_id es nulo o indefinido');
+    }
+  }
+
+
+  private getLatestHydrationForLast24Hours(): void {
+    if (this.user_id) {
+      this.dashboardService.getLatestHydrationLast24Hours(this.user_id).subscribe(
+        totalMl => {
+          if (totalMl !== null && totalMl !== undefined) {
+            this.latestHydrationData = totalMl;
+            this.progress_hyd = this.latestHydrationData * 100 / this.GOAL_HYDRATION;
+          } else {
+            console.warn("No se obtuvieron datos recientes de hidratación en las últimas 24 horas.");
+            this.progress_hyd = 0;
+          }
+        },
+        error => {
+          console.error('Error fetching hydration data:', error);
+          this.progress_hyd = 0;
+        }
+      );
+    } else {
+      this.progress_hyd = 0;
       console.warn('user_id is null or undefined');
     }
 
   }
+
+  protected openModal() {
+
+    this.dashboardService.updateHydration(this.user_id, this.updatingHdr).subscribe(
+      response => {
+        this.updatingHdr = 0;
+        console.log('Registro guardado exitosamente:', response);
+      },
+      error => {
+        this.updatingHdr = 0;
+        console.error('Error al guardar el registro:', error);
+      }
+    );
+
+    this.dashboardService.updateHours(this.user_id, parseInt(String(this.updatingHours)), this.qualitySleep).subscribe(
+      response => {
+        this.updatingHours = 0;
+        this.qualitySleep = '';
+        console.log('Registro guardado exitosamente:', response);
+      },
+      error => {
+        this.updatingHours = 0;  // o 0 si prefieres un número
+        this.qualitySleep = '';
+        console.error('Error al guardar el registro:', error);
+      }
+    );
+
+
+  }
+
 
 }
